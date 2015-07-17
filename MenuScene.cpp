7b486c0099778a6d735ee8a6ef77369bde817325
Lipu-Z <vexillar@hotmail.com>
2015-07-17 14:41:21 +0200

    added menu scene
    This rolls back to commit 7b11f7d34db9e0be51494cb9484609420d48172c.

---
+++ MenuScene.cpp
@@ -0,0 +1,9 @@
+//
+//  MenuScene.cpp
+//  findrabbit
+//
+//  Created by Lipu Zhong on 15/7/17.
+//
+//
+
+#include "MenuScene.h"
---
+++ MenuScene.h
@@ -0,0 +1,14 @@
+//
+//  MenuScene.h
+//  findrabbit
+//
+//  Created by Lipu Zhong on 15/7/17.
+//
+//
+
+#ifndef __findrabbit__MenuScene__
+#define __findrabbit__MenuScene__
+
+#include <stdio.h>
+
+#endif /* defined(__findrabbit__MenuScene__) */
