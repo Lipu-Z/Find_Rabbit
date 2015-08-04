5ea04b51236f77fd495360d9d4afebd69a8b0ad0
Lipu-Z <vexillar@hotmail.com>
2015-08-04 11:13:30 +0200

    gameover layer

--- GameOverLayer.h
+++ GameOverLayer.h
@@ -8,7 +8,7 @@
 
 #ifndef __findrabbit__GameOverLayer__
 #define __findrabbit__GameOverLayer__
-USING_NS_CC
+USING_NS_CC;
 #include <stdio.h>
 class GameOverLayerDelegate {
     virtual void GameOverLayerRestartGame()=0;
@@ -17,11 +17,13 @@ class GameOverLayer:public cocos2d::Layer {
 public:
     virtual bool init();
     CREATE_FUNC(GameOverLayer);
-    void setDelegate(GameOverLayerDelegate *delegate) {m_delegate = delegate;}
+    void setDelegate(GameOverLayerDelegate *delegate) {delegate = delegate;}
     void GameOver (int CurrentScore, int BestScore);
 private:
     void ClickStartGame();
-    Label
-}
+    Label *m_labelTitle;
+    Label *m_labelScoreMsg;
+    GameOverLayerDelegate *m_delegate;
+};
 
 #endif /* defined(__findrabbit__GameOverLayer__) */
