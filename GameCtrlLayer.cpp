2bbb2ac472d8a44a665064e08f4ef486d7e4cc6f
Lipu-Z <vexillar@hotmail.com>
2015-08-21 11:09:56 +0200

    button motion changed

--- GameCtrlLayer.cpp
+++ GameCtrlLayer.cpp
@@ -17,14 +17,14 @@ bool GameCtrlLayer::init(){
     }
     Size visibleSize = Director::getInstance()->getVisibleSize();
     
-    Label *labelBack = Label::createWithSystemFont("back", "", 70);
+    Label *labelBack = Label::createWithSystemFont("back", "", 50);
     MenuItemLabel *menuItemLabel = MenuItemLabel::create(labelBack, CC_CALLBACK_0(GameCtrlLayer::clickEndGame,this));
     Menu *menuEnd = Menu::create(menuItemLabel,NULL);
     menuEnd->setPosition(80,visibleSize.height-50);
     this->addChild(menuEnd);
     
-    //movement of the button
-    menuItemLabel->runAction(RepeatForever::create(Sequence::create(ScaleTo::create(0.5,0.8), ScaleTo::create(0.5, 1.2), NULL)));
+//    //movement of the button
+//    menuItemLabel->runAction(RepeatForever::create(Sequence::create(ScaleTo::create(0.5,0.8), ScaleTo::create(0.5, 1.2), NULL)));
     return true;
 }
 
